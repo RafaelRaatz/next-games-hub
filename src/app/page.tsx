3300b2a31c1d6f07af00fe83929a220ca8d6df92
Rafael Raatz <mrrafa.raatz@gmail.com>
2024-12-02 15:29:02 -0300

    :sparkles: feat: new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,55 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/games";
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRightSquare } from "react-icons/bs";
 
-export default function Home() {
+async function getRandomGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+    );
+
+    return res.json();
+  } catch (err) {
+    throw new Error("failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const randomGame: GameProps = await getRandomGame();
+
   return (
-   <div>
-    <h1>Hello world</h1>
-   </div>
+    <main className="w-full ">
+      <Container>
+        <Link href={`/game/${randomGame.id}`}>
+          <div className="w-full bg-black rounded-lg mt-8">
+            <div className="w-full max-h-96 h-96 relative rounded-lg ">
+              <div className="absolute z-20 justify-center items-center right-0 bottom-0 p-6">
+                <h1 className="text-center font-bold text-4xl text-white mb-4">
+                  We have an exclusive <br /> game for you!
+                </h1>
+                <div className="flex gap-2 items-center justify-center">
+                  <p className="font-bold text-xl text-white">
+                    {randomGame.title}
+                  </p>
+                  <BsArrowRightSquare size={24} color="#fff" />
+                </div>
+              </div>
+
+              <Image
+                src={randomGame.image_url}
+                alt={randomGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-30 hover:opacity-60 transiton-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+              />
+            </div>
+          </div>
+        </Link>
+      </Container>
+    </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,12 +3,12 @@ import { LiaGamepadSolid } from "react-icons/lia";
 
 export function Header() {
   return (
-    <header className="w-full h-28 bg-slate-300 text-black px-2">
+    <header className="w-full h-28 bg-black text-white px-2">
       <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
         <div className="flex justify-center items-center gap-4">
           <Link href="/">
-            <h1 className="text-4xl">
-              Games<span className="text-purple-600 font-bold">+</span>
+            <h1 className="text-4xl flex gap-1 font-bold ">
+              GameBoard<span className="text-purple-600 ">+</span>
             </h1>
           </Link>
           <div className="flex gap-4 justify-center items-center pt-2">
