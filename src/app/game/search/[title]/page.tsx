ae8bfb095b0272d230195197d2643bcfcb5e6816
Rafael Raatz <mrrafa.raatz@gmail.com>
2024-12-02 19:14:04 -0300

    :sparkles: feat: add search page

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,38 @@
+import { Card } from "@/components/card";
+import { Container } from "@/components/container";
+import { Input } from "@/components/input";
+import { GameProps } from "@/utils/types/games";
+
+async function getData(title: string) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`
+    );
+    return res.json();
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function Search({
+  params: { title },
+}: {
+  params: { title: string };
+}) {
+  const games: GameProps[] = await getData(title);
+
+  return (
+    <div className="w-full text-black">
+      <Container>
+        <Input />
+        <h1 className="font-bold text-xl mt-8 mb-5">Games founded</h1>
+
+        {!games && <p>game not found...</p>}
+
+        <div className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {games && games.map((item) => <Card key={item.id} data={item} />)}
+        </div>
+      </Container>
+    </div>
+  );
+}
